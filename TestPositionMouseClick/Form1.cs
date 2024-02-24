94ea129dbd5935df872ad26d5e3c706858ed254e
Dongyang5858 <duongchung1999@gmail.com>
2024-02-24 17:09:18 +0700

    mouse click

---
+++ TestPositionMouseClick/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestPositionMouseClick
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(ref Point lpPoint);
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var pt = new Point();
+
+            GetCursorPos(ref pt);
+            txtXPos.Text = pt.X.ToString();
+            txtYPos.Text = pt.Y.ToString();
+        }
+    }
+}
